0b610624ebbe5c2ffc538e496130557e3d4488e3
christine-vaug <christinev@nevada.unr.edu>
2019-09-24 14:52:56 -0700

    tried making dragon work

--- PA4/src/object.cpp
+++ PA4/src/object.cpp
@@ -255,6 +255,7 @@ bool Object::loadOBJ(char* path, std::vector<glm::vec3> & out_vertices,
 	std::vector<glm::vec3> temp_vertices; 
 	std::vector<glm::vec2> temp_uvs;
 	std::vector<glm::vec3> temp_normals;
+  bool blenderFormat = 1; //if first format works, blenderFormat = 1, else blenderFormat = 0;
 
 	FILE * file = fopen(path, "r");
 	if( file == NULL ){
@@ -287,23 +288,38 @@ bool Object::loadOBJ(char* path, std::vector<glm::vec3> & out_vertices,
 			fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
 			temp_normals.push_back(normal);
 		}else if ( strcmp( lineHeader, "f" ) == 0 ){
+      fpos_t pos; //holds current position
+      fgetpos (file,&pos); //set position to beginning of line
 			std::string vertex1, vertex2, vertex3;
 			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
-			if (matches != 9){
-				printf("File can't be read by our simple parser :-( Try exporting with other options\n");
-				fclose(file);
-				return false;
+			int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
+                            &vertexIndex[0], &uvIndex[0], &normalIndex[0],
+                            &vertexIndex[1], &uvIndex[1], &normalIndex[1],
+                            &vertexIndex[2], &uvIndex[2], &normalIndex[2] );
+			if (matches != 9) //if this didn't work, then we need to read a different format
+      {
+        blenderFormat = 0;
+        fsetpos (file,&pos); //return position in file to beginning of the line
+        matches = fscanf(file, "%d %d %d\n", &vertexIndex[0], &vertexIndex[1], &vertexIndex[2]);
+        if(matches !=3) //if this didn't work, something else went wrong
+        {
+          printf("Something went wrong.\n");
+          fclose(file);
+          return false;
+        }
 			}
 			vertexIndices.push_back(vertexIndex[0]);
 			vertexIndices.push_back(vertexIndex[1]);
 			vertexIndices.push_back(vertexIndex[2]);
-			uvIndices    .push_back(uvIndex[0]);
-			uvIndices    .push_back(uvIndex[1]);
-			uvIndices    .push_back(uvIndex[2]);
-			normalIndices.push_back(normalIndex[0]);
-			normalIndices.push_back(normalIndex[1]);
-			normalIndices.push_back(normalIndex[2]);
+      if(blenderFormat)
+      {
+        uvIndices    .push_back(uvIndex[0]);
+        uvIndices    .push_back(uvIndex[1]);
+        uvIndices    .push_back(uvIndex[2]);
+        normalIndices.push_back(normalIndex[0]);
+        normalIndices.push_back(normalIndex[1]);
+        normalIndices.push_back(normalIndex[2]);
+      }
 		}else{
 			// Probably a comment, eat up the rest of the line
 			char stupidBuffer[1000];
@@ -317,18 +333,31 @@ bool Object::loadOBJ(char* path, std::vector<glm::vec3> & out_vertices,
 
 		// Get the indices of its attributes
 		unsigned int vertexIndex = vertexIndices[i];
-		unsigned int uvIndex = uvIndices[i];
-		unsigned int normalIndex = normalIndices[i];
+    unsigned int uvIndex;
+    unsigned int normalIndex;
+    if(blenderFormat)
+    {
+      uvIndex = uvIndices[i];
+      normalIndex = normalIndices[i];
+    }
 		
 		// Get the attributes thanks to the index
 		glm::vec3 vertex = temp_vertices[ vertexIndex-1 ];
-		glm::vec2 uv = temp_uvs[ uvIndex-1 ];
-		glm::vec3 normal = temp_normals[ normalIndex-1 ];
+    glm::vec2 uv;
+    glm::vec3 normal;
+    if(blenderFormat)
+    {
+      uv = temp_uvs[ uvIndex-1 ];
+      normal = temp_normals[ normalIndex-1 ];
+    }
 		
 		// Put the attributes in buffers
 		out_vertices.push_back(vertex);
-		out_uvs     .push_back(uv);
-		out_normals .push_back(normal);
+    if(blenderFormat)
+    {
+      out_uvs     .push_back(uv);
+      out_normals .push_back(normal);
+    }
 	
 	}
 	fclose(file);
